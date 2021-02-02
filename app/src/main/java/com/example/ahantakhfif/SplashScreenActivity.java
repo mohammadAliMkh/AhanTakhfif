ac3054e40c0bd3f6c82101a655dfce94776b0c6a
mamk434 <ma.mohammadkhani1373@gmail.com>
2021-02-02 23:07:30 +0330

    add SplashScreenActivity

--- app/src/main/java/com/example/ahantakhfif/SplashScreenActivity.java
+++ app/src/main/java/com/example/ahantakhfif/SplashScreenActivity.java
@@ -3,12 +3,19 @@ package com.example.ahantakhfif;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+
     }
 }
