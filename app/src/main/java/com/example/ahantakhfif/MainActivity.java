5f64bf611d8bbac45bc4480721e24e4258ac97c8
mamk434 <ma.mohammadkhani1373@gmail.com>
2021-02-03 12:57:05 +0330

    create button in main and action bar

--- app/src/main/java/com/example/ahantakhfif/MainActivity.java
+++ app/src/main/java/com/example/ahantakhfif/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.ahantakhfif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button fansWeightBtn;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+        fansWeightBtn = findViewById(R.id.btn_fans_weight_calculator);
+
+        fansWeightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , WeightCalculatorActivity.class );
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
--- app/src/main/java/com/example/ahantakhfif/SplashScreenActivity.java
+++ app/src/main/java/com/example/ahantakhfif/SplashScreenActivity.java
@@ -13,9 +13,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
 
